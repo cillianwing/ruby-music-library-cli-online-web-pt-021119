d9aba391a566a16fc963ad7ad7f4314ad69ef89d
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-04-02 02:14:15 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -47,7 +47,9 @@ class MusicLibraryController
     input = gets.strip
     if artist = Artist.find_by_name(input)
       Artist.songs.sort_by(&:name).each_with_index do |x, index|
-        
+        puts "#{index+1}. #{x.song.name} - #{x.genre.name}"
+      end
+    end
     #binding.pry
   end
 
