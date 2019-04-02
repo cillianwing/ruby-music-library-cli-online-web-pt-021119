13bf804c8149c73d15b3eac8f933d7ddcc3ffff3
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-04-02 02:29:39 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -67,6 +67,7 @@ class MusicLibraryController
     input = gets.strip.to_i
     #binding.pry
     if (1..Song.all.length).include?(input)
+      song = 
       puts "Now playing #{} by #{}"
     else 
       puts "Which song number would you like to play?"
